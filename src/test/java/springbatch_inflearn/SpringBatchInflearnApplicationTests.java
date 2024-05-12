ac9b463471753967c8f997c9fa03ae26797a3fe4
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2024-05-13 00:33:38 +0900

    Feat : 스프링배치 초기설정

---
+++ src/test/java/springbatch_inflearn/SpringBatchInflearnApplicationTests.java
@@ -0,0 +1,13 @@
+package springbatch_inflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchInflearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
