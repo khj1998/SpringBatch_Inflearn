0185b94802d816318f1cc0affd112b08cee28746
Quokka_khj <80220062+khj1998@users.noreply.github.com>
2024-05-23 11:41:22 +0900

    Feat : Flow 생성

--- src/main/java/springbatch_inflearn/jobconfig/JobConfiguration.java
+++ src/main/java/springbatch_inflearn/jobconfig/JobConfiguration.java
@@ -5,7 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecutionListener;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.FlowBuilder;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -34,6 +36,23 @@ public class JobConfiguration {
                 .build();
     }
 
+    @Bean
+    public Job job2() {
+        return new JobBuilder("repository_test",jobRepository)
+                .start(flow())
+                .next(step5())
+                .end()
+                .build();
+    }
+
+    private Flow flow() {
+        FlowBuilder<Flow> flowBuilder = new FlowBuilder<>("flow");
+
+        return flowBuilder.start(step3())
+                .next(step4())
+                .end();
+    }
+
     private Step step() {
         return new StepBuilder("start-step",jobRepository)
                 .tasklet(tasklet(),platformTransactionManager)
@@ -43,6 +62,24 @@ public class JobConfiguration {
     private Step step2() {
         return new StepBuilder("step2",jobRepository)
                 .tasklet(tasklet2(),platformTransactionManager)
+                .build();
+    }
+
+    private Step step3() {
+        return new StepBuilder("start-step",jobRepository)
+                .tasklet(tasklet(),platformTransactionManager)
+                .build();
+    }
+
+    private Step step4() {
+        return new StepBuilder("step2",jobRepository)
+                .tasklet(tasklet2(),platformTransactionManager)
+                .build();
+    }
+
+    private Step step5() {
+        return new StepBuilder("step2",jobRepository)
+                .tasklet(tasklet3(),platformTransactionManager)
                 .build();
     }
 
@@ -57,6 +94,13 @@ public class JobConfiguration {
     private Tasklet tasklet2() {
         return ((contribution, chunkContext) -> {
             log.info("step2 - tasklet2 수행");
+            return RepeatStatus.FINISHED;
+        });
+    }
+
+    private Tasklet tasklet3() {
+        return ((contribution, chunkContext) -> {
+            log.info("step5 - flow job 수행");
             return RepeatStatus.FINISHED;
         });
     }
